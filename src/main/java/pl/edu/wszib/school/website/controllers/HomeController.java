e227d4794985a4e14accbece67c0c33e7db3cf0d
nike1994 <kozak-wiktoria@wp.pl>
2021-02-01 18:53:03 +0100

    front-end

--- src/main/java/pl/edu/wszib/school/website/controllers/HomeController.java
+++ src/main/java/pl/edu/wszib/school/website/controllers/HomeController.java
@@ -29,8 +29,11 @@ public class HomeController {
             }});
             put("site4","site4");
         }};
-        
+
 // TODO: 31.01.2021 trzeba stworzyć możliwość edycji tekstowej zawartości strony
+// TODO: 01.02.2021 stworzenie strony dla administratora/ dodanie usuwanie edycja stron / dodanie usuwanie edycja uczniów, nauczycieli, rodziców
+// TODO: 01.02.2021 stworzenie strony dla nauczyciela edycja postu/ dodanie usuwanie stron z postami/
+
 
         model.addAttribute("sites",sites);
         model.addAttribute("telephons",telephon);
