e2422aa0fe2470dc84221bb908d0b18f03742b14
nike1994 <kozak-wiktoria@wp.pl>
2021-02-14 21:52:37 +0100

    DB

--- src/main/java/pl/edu/wszib/school/website/configuration/AppConfiguration.java
+++ src/main/java/pl/edu/wszib/school/website/configuration/AppConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+import java.sql.*;
+
 @Configuration
 @ComponentScan("pl.edu.wszib.school.website")
 
@@ -14,6 +16,7 @@ public class AppConfiguration {
 
     @Bean
     public SessionFactory sessionFactory() {
+        createDBifExist();
         return new org.hibernate.cfg.Configuration().configure().buildSessionFactory();
     }
 
@@ -30,6 +33,53 @@ public class AppConfiguration {
         secondaryTemplateResolver.setCheckExistence(true);
 
         return secondaryTemplateResolver;
+    }
+
+
+
+    public void createDBifExist(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost", "root", "");
+            String DBname = "schoolSite";
+            if(!(checkDatabaseExist(DBname,connection))){
+                String sql = "CREATE DATABASE IF NOT EXISTS "+DBname+" ;";
+                Statement statement = connection.createStatement();
+                statement.executeUpdate(sql);
+            }
+
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+
+    public boolean checkDatabaseExist(String name, Connection connection){
+        Statement statement =null;
+
+        try {
+
+            statement = connection.createStatement();
+
+            DatabaseMetaData meta = connection.getMetaData();
+            ResultSet resultSet = meta.getCatalogs();
+
+            while(resultSet.next()){
+                String DBName = resultSet.getString(1).toLowerCase();
+                if(DBName.equals(name.toLowerCase())){
+                    System.out.println(true);
+                    return true;
+                }
+            }
+
+        } catch ( SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(false);
+        return false;
     }
 
 }
